39d4e2337c8e014138f94c4f568a442f569edfe1
deepanshu1334 <106024910+deepanshu1334@users.noreply.github.com>
2022-08-31 23:36:14 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,56 @@
+#1 write a python script to add comments and print "learning python"
+#deepanshu single line comment
+'''deepanshu'''#multiline comment
+print("learning python",end=" ")
+
+#2write a python script to add multi line comments and print values of four variables,each in new line,variable contain any value
+
+a="deepanshu"
+b=3.45
+c=34
+z=True
+print(a,b,c,z,sep="\n")
+
+#3write a python script to print values of a variable,create 5 variable each of them contain differnt type of data
+a="deepanhshu"
+b=3.45
+c=True
+d=3+5j
+e=int
+print(a,b,c,d,e,end=" ")
+
+#4 write a python script to print the id of two variables containin the same integer values
+print()
+a=5
+b=5
+print(id(a),id(b),sep="\n")
+
+#5
+
+'''a="deepanshu"
+b="mysirg"
+c=3.56 
+d=3+5j
+print(type(a),id(a))
+print(type(b,id(b)))
+print(type(c),id(c))
+print(type(d),id(d))
+print()
+print(help())'''
+
+#7
+a=True 
+b=False
+c=None
+print(a,b,c,end=" ")
+print()
+
+#8
+date=31
+month=8
+year=2022
+print(date,month,year,sep="-")
+time=11
+minute=30
+s="PM"
+print(time,minute,sep="::")

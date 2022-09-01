f303e79a9645705d9b748b482b032b37ea7cb6df
deepanshu <dk0962422@gmail.com>
2022-08-31 22:12:15 -1200

    initial commit

---
+++ ASSIGNMENT1.py
@@ -0,0 +1,47 @@
+#write a command to get python version you are using
+
+#python --version
+
+#2 hello python
+from re import A
+
+
+print("hello python")
+
+#3 command line on window power shell
+#python a1.py
+
+#4
+print("hello python")
+
+#4
+print("hello ")
+print("python")
+
+#5
+print("\"mysirg\"")
+
+#6
+print("""\"Teachers's day\"""")
+
+#7
+a=5
+print(a,end=" ")
+
+#8
+
+a="deepanshu"
+b=20
+c="btech"
+d=1
+print(a,b,c,d,end=" ")
+print()
+
+#10
+
+a=10
+b="deepanshi"
+c=3.54
+print(a)
+print(b)
+print(c)

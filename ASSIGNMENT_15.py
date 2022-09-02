ebe33487ef0d882bac004b962199506172bd95df
deepanshu1334 <106024910+deepanshu1334@users.noreply.github.com>
2022-09-02 21:59:29 +0530

    Add files via upload

---
+++ ASSIGNMENT_15.py
@@ -0,0 +1,51 @@
+#to crete string in 3 diffrent ways
+
+s1="mysirg"
+s2="""deepanshu"""
+s3='''champion'''
+
+#2
+#slice program
+s1="ineuron"
+print(s1[:5])
+
+#3
+s2="hello learners"
+print(s2[2:5]) #stp by default one maan lega,
+
+#4
+a="learning"
+b="python"
+c=a+b
+print(c)
+
+#6
+s1="ineuron"
+print(s1.count("e"))
+print()
+#strin reverser
+print(s1[::-1]) #begging value 0 hogi aur end value 0 by default
+print()#string reverse ho jaege ye
+
+#7
+
+print(s1.index("in"))
+
+#8
+s1="12343"
+print(s1.isdigit())
+
+#8
+
+s1="1a2b3c4c"
+print(s1.isalnum())
+print(s1.isalpha())
+
+#10
+
+a=6
+b=str(a)
+print(b)
+print(type(b))
+
+#

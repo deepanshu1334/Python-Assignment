d5616ce16248d09c42a6dba1a82f271f2689c7ce
deepanshu1334 <106024910+deepanshu1334@users.noreply.github.com>
2022-09-08 20:02:52 +0530

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,74 @@
+#1
+
+'''x=input("enter your name")
+print(x)
+print()'''
+
+#2
+
+'''x=int(input())
+print(x)
+print()'''
+
+
+#3
+
+'''a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=a+b
+print(c)
+print()'''
+
+#4
+
+'''r=int(input("enter the radius"))
+area=3.14*r*r
+print(area)'''
+
+#5
+
+'''n=int(input("Enter the number"))
+area=n*n
+print(area)'''
+
+#6
+
+'''a=int(input("Etner teh number"))
+b=int(input("enter teh numebr"))
+area=1*a*b/2
+print(area)
+print()'''
+
+#7
+
+'''a=int(input("Etner the number"))
+b=int(input("enter teh number"))
+c=int(input("enter the number"))
+avg=(a+b+c)/3
+print(avg)
+print()'''
+
+#8
+
+'''p=int(input("enter the principal"))
+r=int(input("Enter the rate"))
+t=int(input("enter teh time"))
+si=(p*r*t)/100
+print(si)'''
+
+#9
+
+
+
+
+l,b,h=2,3,4
+volume=l*b*h
+print(volume)
+
+#10
+
+'''l=int(input("enter the number"))
+b=int(input("Enter the numbere"))
+area=l*b
+print(area)
+print()'''

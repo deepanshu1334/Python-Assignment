588d5c9fa6ae260111d214d2733293e4df418088
deepanshu1334 <106024910+deepanshu1334@users.noreply.github.com>
2022-09-09 00:30:52 +0530

    Add files via upload

---
+++ Assignment_5.py
@@ -0,0 +1,65 @@
+#1 to get the last digit
+
+'''a=int(input("enter the numvber"))
+b=a%10 #to get last digit we use modulus operator
+c=a//10
+print(b)
+print(c)
+print()'''
+
+#2
+
+'''a=int(input("enter the number"))
+b=a%10
+print(b)'''
+
+#3
+
+'''a=int(input("enter the number"))
+b=int(input("enter the number"))
+a,b=b,a
+print(a,b)'''
+
+#4
+
+'''x=int(input("enter the number"))
+y=x**x
+print(y)
+print()'''
+
+#5 display only its first digit
+'''a=int(input("Enter the number"))
+b=a//10
+c=b//10
+print(c)
+print()'''
+
+#6 display only its middle digit
+'''a=int(input("Enter the number"))
+b=a//10
+c=b%10
+print(c)
+print()'''
+
+#7
+
+'''a=[3,4,5,6]
+b=34 in a
+c=402 in a
+print(c)
+print(b)
+print(34 in a)'''
+
+
+#8
+
+'''a=[34,56,4,1]
+print(34 not in a)'''
+
+
+#10
+
+'''a=5
+b=5
+print(a is b)'''
+

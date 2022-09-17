0b11577368d66ba11782be6bcfe42d479894f768
Deepanshu <106024910+deepanshu1334@users.noreply.github.com>
2022-09-17 20:45:14 +0530

    Add files via upload

---
+++ Assignment_9.py
@@ -0,0 +1,100 @@
+#1
+'''n=int(input("enter the number"))
+i=1
+while i<=n:
+    print("mysirg")
+    i+=1
+print()    '''
+
+#2
+
+'''n=int(input("enter the number"))
+i=1
+while i<=n:
+    print(i)
+    i+=1
+print()    '''
+
+#3
+
+'''i=1
+n=int(input("enter the number"))
+while n>=i:
+    print(n)
+    n-=1
+print()'''
+
+#4
+
+'''n=int(input("Enter the number"))
+i=1
+while i<=n:
+    print(2*i-1)
+    i+=1
+
+print()    '''
+
+#5
+
+'''n=int(input("enter the number"))
+i=1
+
+while n>=i:
+    print(2*n-1)
+    n-=1
+
+print()'''
+
+#6
+
+'''n=int(input("Enter the number"))
+i=1
+while i<=n:
+    print(2*i)
+    i+=1
+
+print()    '''
+
+#7
+
+'''n=int(input("Enter the number"))
+i=1
+
+while i<=n:
+    print(2*n-1)
+    n-=1
+
+print()'''
+
+#8
+
+'''i=1
+n=int(input("enter the number"))
+while i<=n:
+    print(i*i)
+    i+=1
+
+print()'''
+
+#9
+
+'''n=int(input("enter the number"))
+i=1
+while i<=n:
+    print(i*i*i)
+    i+=1
+
+print()    '''
+
+#10
+n=int(input("enter hte number"))
+i=1
+while i<=n:
+    print("nq",i)
+    i+=1
+
+print()
+
+
+
+

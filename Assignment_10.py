ba361fc6b73bcc3e52c50acc356b45b7903293a3
Deepanshu <106024910+deepanshu1334@users.noreply.github.com>
2022-09-17 21:25:33 +0530

    Add files via upload

---
+++ Assignment_10.py
@@ -0,0 +1,56 @@
+#1
+
+'''n=int(input("Enter the number"))
+i=1
+for e in range(1,6):
+    print("mysirg")
+print()'''
+
+#2
+
+'''for e in range(int(input("enter the number"))):
+    print(e+1)'''
+
+#3
+
+'''n=int(input("Enter the number"))
+for e in range(n,0,-2):
+    print(e)'''
+
+'''n=int(input("Enter the number"))
+for e in range(1,n,2):
+    print(e)'''
+
+#6
+
+'''n=int(input("enter the number"))
+for e in range(2,n,2):
+    print(e)'''
+
+#7
+
+'''n=int(input("enter the number"))
+i=1
+for e in range(n,i,i):
+    print(e)    '''
+
+#8 
+'''n=int(input("enter the number"))
+for e in range(1,n):
+    print(e*e)'''
+
+#9
+
+'''n=int(input("enter the number"))
+for e in range(1,n):
+    print(e*e*e)
+print()        '''
+
+#10
+
+'''n=int(input("enter the number"))
+for e in range(1,n):
+    print("nq",e)
+print()'''    
+
+

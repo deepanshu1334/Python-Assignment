bc5496d99ccb727061dfff252f809e31c81fb0d1
Deepanshu <106024910+deepanshu1334@users.noreply.github.com>
2022-09-17 19:58:11 +0530

    Add files via upload

---
+++ ASSIGNMENT_8.py
@@ -0,0 +1,68 @@
+#to print musirg 5 times
+'''i=1
+while i<=5:
+    print("mysirg")
+    i=i+1
+print()'''    
+
+'''i=1
+while i<=10:
+    print(i,end=" ")
+    i+=1
+print()'''
+
+#print natural no in reverse order
+'''i=1
+while i<=10:
+    print(11-i) #print(i)
+    i+=1'''
+
+'''#print first 10 odd natural no 
+n=int(input("Enter the number"))
+i=1
+while i<=n:
+    print(2*i-1)
+    i+=1
+print() ''' 
+
+'''#to print first 10 odd natural no in reverse order
+n=int(input("enter the number"))
+i=10
+while i>=n:
+    print(2*i-1)
+    i-=1
+print()'''
+
+
+#first 10 even natural number
+'''i=1
+n=int(input("Enter the number"))
+while i<=n:
+    print(2*i)
+    i+=1
+print() '''   
+
+'''#reverse order
+i=10
+n=int(input("enter the number"))
+while i>=n:
+    print(2*i)
+    i-=1
+print() '''   
+
+'''#square of natural  numbers
+i=1
+n=int(input("enter the number"))
+while i<=n:
+    print(i*i)
+    i+=1
+print()'''
+
+#to print multiple of 5
+'''
+i=1
+n=int(input("enter the number"))
+while i<=n:
+    print(5*i)
+    i+=1
+print()'''
